63173ace892c81f242f8c2ec3980496e9adf5390
Miguel Ramirez <miguelramirez88@.hotmail.com>
2021-11-21 13:34:00 -0600

    Agregado el filtro y reordenamiento de la tabla

--- src/components/PxAssetsTable.vue
+++ src/components/PxAssetsTable.vue
@@ -3,17 +3,25 @@
     <thead>
       <tr class="bg-gray-100 border-b-2 border-gray-400">
         <th></th>
-        <th>
-          <span>Ranking</span>
+        <th :class="{ up: this.sortOrder === 1, down: this.sortOrder === -1 }">
+          <span class="underline cursor-pointer" @click="changeSortOrder">Ranking</span>
         </th>
         <th>Nombre</th>
         <th>Precio</th>
         <th>Cap. de Mercado</th>
         <th>Variación 24hs</th>
-        <td class="hidden sm:block"></td>
+        <td class="hidden sm:block">
+          <input
+            class="bg-gray-100 focus:outline-none border-b border-gray-400 py-2 px-4 block w-full appearance-none leading-normal"
+            id="filter"
+            placeholder="Buscar..."
+            type="text"
+            v-model="filter"
+          />
+        </td>
       </tr>
     </thead>
-    <tbody v-for="a in assets" v-bind:key="a.id">
+    <tbody v-for="a in filteredAssets" v-bind:key="a.id">
       <tr
         class="border-b border-gray-200 hover:bg-gray-100 hover:bg-orange-100"
       >
@@ -62,6 +70,34 @@ import PxButton from '@/components/PxButton'
 export default {
   name: 'PxAssetsTable',
 
+  data(){
+    return{
+      filter: '',
+      sortOrder: 1
+    }
+  },
+
+  computed: {
+    filteredAssets(){
+      // if (!this.filter) {return this.assets}
+
+      const altOrder = this.sortOrder === 1 ? -1 : 1
+
+      return this.assets.filter(a => 
+        a.symbol.toLowerCase().includes(this.filter.toLowerCase()) ||
+        a.name.toLowerCase().includes(this.filter.toLowerCase())
+        )
+        .sort((a,b) => {
+          if (parseInt(a.rank) > parseInt(b.rank)) {
+            return this.sortOrder
+          }
+
+          return altOrder
+        })
+
+    }
+  },
+
   components: { PxButton },
 
   props: {
@@ -75,7 +111,10 @@ export default {
   methods: {
     goToCoin(id){
       this.$router.push({ name: 'coin-detail', params: { id } });
-      }
+      },
+    changeSortOrder(){
+      this.sortOrder = this.sortOrder === 1 ? -1 : 1
+    }
   }
 }
 </script>
